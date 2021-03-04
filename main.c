826d794805dc8e68ee16ea870352c43fa0b564d3
fncbarros <barrosfnc@gmail.com>
2021-03-04 15:38:16 +0000

    ignr

--- main.c
+++ main.c
@@ -3,17 +3,5 @@
 
 int main()
 {
-	void *p;
-//	char s[] = {65, 66, 67, 68, 69, 0, 45};
-//	char s0[] = { 0,  0,  0,  0,  0,  0, 0};
-//	char sCpy[] = {65, 66, 67, 68, 69, 0, 45};
-	char sResult[] = {67, 68, 67, 68, 69, 0, 45};
-	char sResult2[] = {67, 67, 68, 68, 69, 0, 45};	
-
-	p = ft_memmove(sResult + 1, sResult, 2);
-	int i = 0;
-	while (((unsigned char*)p)[i]){
-		printf("%d ", ((unsigned char *)p)[i++]);
-	}
-	printf("\n%d", ft_memcmp(sResult, sResult2, 7));
+	test;
 }
