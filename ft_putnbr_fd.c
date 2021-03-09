352c12509e686d933d1df6f2fe4a96b8449209a3
fncbarros <barrosfnc@gmail.com>
2021-03-09 22:33:21 +0000

    putnbr correction

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: fbarros <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 17:34:14 by fbarros           #+#    #+#             */
-/*   Updated: 2021/03/05 23:20:59 by fbarros          ###   ########.fr       */
+/*   Updated: 2021/03/09 21:44:49 by fbarros          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,16 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*nbr;
+	unsigned int	nbr;
 
-	nbr = ft_itoa(n);
-	ft_putstr_fd(nbr, fd);
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr = n * (-1);
+	}
+	else
+		nbr = n;
+	if (nbr > 9)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd(nbr % 10 + '0', fd);
 }
