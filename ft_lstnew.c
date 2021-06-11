ad9f649953058e51ec12724c7ab6964ceb04ec2e
Francisco Barros <fbarros@student.42lisboa.com>
2021-06-11 17:55:31 +0100

    minor correction

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: fbarros <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/26 11:59:45 by fbarros           #+#    #+#             */
-/*   Updated: 2021/03/12 19:42:28 by fbarros          ###   ########.fr       */
+/*   Updated: 2021/06/11 17:55:11 by fbarros          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstnew(void *content)
 	t_list	*list;
 
 	list = NULL;
-	list = malloc(sizeof(content) + sizeof(void *));
+	list = malloc(sizeof(t_list));
 	if (!list)
 		return (NULL);
 	list->next = NULL;
