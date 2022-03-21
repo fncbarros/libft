bedf3495b6747a6bc499bf05890467a1a0ed79a5
fbarros <fbarros@student.42lisboa.com>
2022-03-21 15:40:03 +0000

    strset needs correction maybe

--- ft_strrset.c
+++ ft_strrset.c
@@ -1,27 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strset.c                                        :+:      :+:    :+:   */
+/*   ft_strrset.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbarros <fbarros@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/16 12:34:40 by fbarros           #+#    #+#             */
-/*   Updated: 2022/03/21 15:38:41 by fbarros          ###   ########.fr       */
+/*   Created: 2022/03/21 15:10:44 by fbarros           #+#    #+#             */
+/*   Updated: 2022/03/21 15:38:00 by fbarros          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strset(const char *s, char *set)
+char	*ft_strrset(const char *s, char *set)
 /**
- * Finds first instance of any char of set in s.
+ * Finds last instance of any char in set within s.
 */
 {
+	char	*ptr1;
+	char	*ptr2;
+
+	ptr1 = NULL;
+	ptr2 = NULL;
 	while (*set)
 	{
-		if (ft_strchr(s, *set))
-			return (ft_strchr(s, *set));
+		ptr1 = ft_strrchr(s, *set);
+		if (ptr1 > ptr2)
+			ptr2 = ptr1;
 		set++;
 	}
-	return (NULL);
+	return (ptr2);
 }
